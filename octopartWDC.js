2f4099ea5987da05f26807eb14aeff708f9d332a
OctopartTester <e356975@us.lmco.com>
2018-07-02 10:21:15 -0600

    working version

--- octopartWDC.js
+++ octopartWDC.js
@@ -1,46 +1,59 @@
-(function () {
+(function() {
+    // Create the connector object
     var myConnector = tableau.makeConnector();
 
-    myConnector.getSchema = function (schemaCallback) {
-    var cols = [{
-        id: "hits",
-        dataType: tableau.dataTypeEnum.string
-    }];
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "uid",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "name",
+            alias: "manufacturer",
+            dataType: tableau.dataTypeEnum.string
+        }];
 
-    var tableSchema = {
-        id: "OctopartData",
-        alias: "This is a search for the part with mpn of SN74S74N",
-        columns: cols
+        var tableSchema = {
+            id: "octopart",
+            alias: "Part Data with texas in the name",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
     };
 
-    schemaCallback([tableSchema]);
-};
-
+    // Download the data
     myConnector.getData = function(table, doneCallback) {
-    $.getJSON("https://octopart.com/api/v3/parts/match?apikey=80dfab31&queries=[{%22mpn%22:%22SN74S74N%22}]&pretty_print=true", function(resp) {
-        var feat = resp.features,
-            tableData = [];
 
-        // Iterate over the JSON object
-        for (var i = 0, len = feat.length; i < len; i++) {
-            tableData.push({
-                "hits": feat[i],
-                "mag": feat[i].properties.mag,
-                "title": feat[i].properties.title,
-                "location": feat[i].geometry
-            });
-        }
+        $.getJSON("https://octopart.com/api/v3/parts/search?q=texas&apikey=80dfab31", function(resp) {
+            var feat = resp.results,
+                tableData = [];
+                 
+                 
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                
+                tableData.push({
 
-        table.appendRows(tableData);
-        doneCallback();
-    });
-};
+                    "uid": feat[i].item.brand.uid,
+                    "name": feat[i].item.manufacturer.name
+                   
+                });
+                tableau.log('after te push')
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
 
     tableau.registerConnector(myConnector);
-    $(document).ready(function () {
-    $("#submitButton").click(function () {
-        tableau.connectionName = "Octopart Data";
-        tableau.submit();
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "Octopart Data"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
     });
-});
 })();
