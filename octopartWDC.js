97af72ec87b26a4501db0b295f93f6b258fe53d0
OctopartTester <e356975@us.lmco.com>
2018-07-16 10:19:19 -0600

    multiple calls

--- octopartWDC.js
+++ octopartWDC.js
@@ -4,7 +4,7 @@
 
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
-        var cols = [{
+        var colsOne = [{
             id: "mpn",
             dataType: tableau.dataTypeEnum.string
         },{
@@ -27,13 +27,42 @@
             dataType: tableau.dataTypeEnum.string
         }];
 
-        var tableSchema = {
-            id: "octopart",
+        var tableSchemaOne = {
+            id: "tableOne",
             alias: "Part Data Search by MPN",
-            columns: cols
+            columns: colsOne
         };
 
-        schemaCallback([tableSchema]);
+         var colsTwo = [{
+            id: "mpn",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "brand_name",
+            alias: "brand",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "manufacturer",
+            
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "sku",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "seller_name",
+            alias: "Seller Name",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "USD",
+            dataType: tableau.dataTypeEnum.string
+        }];
+
+        var tableSchemaTwo = {
+            id: "tableTwo",
+            alias: "Part Data Search by MPN 2",
+            columns: colsTwo
+        };
+
+        schemaCallback([tableSchemaOne, tableSchemaTwo]);
     };
 
     // Download the data
@@ -45,58 +74,91 @@
             partFour = mpnObj.partFour, 
             partFive = mpnObj.partFive;
             // apiCall = "https://octopart.com/api/v3/parts/match?apikey=80dfab31&include%5b%5d=specs,compliance_documents,&callback=?&queries=%5b%7b%22mpn%22:%22"+partOne+"%22%7d,"
-            apiCall = "https://octopart.com/api/v3/parts/match?apikey=80dfab31&queries=[{%22mpn%22:%22"+partOne+"%22}"
+            apiCallOne = "http://octopart.com/api/v3/parts/search?apikey=80dfab31&q=%27"+partOne+ "%27"
             // apiCall = "https://octopart.com/api/v3/parts/match?apikey=80dfab31&queries=%5b%7b%22mpn%22:%22"+partOne+"%22%7d%";
-            if (partTwo){
-                apiCall += ",{%22mpn%22:%22"+partTwo+"%22}"
-            }
-            if (partThree){
-                apiCall += ",{%22mpn%22:%22"+partThree+"%22}"
-            }
-            if (partFour){
-                apiCall += ",{%22mpn%22:%22"+partFour+"%22}"
-            }
-            if (partFive){
-                apiCall += ",{%22mpn%22:%22"+partFive+"%22}"
-            }
+            
+            // if (partThree){
+            //     apiCall += ",{%22mpn%22:%22"+partThree+"%22}"
+            // }
+            // if (partFour){
+            //     apiCall += ",{%22mpn%22:%22"+partFour+"%22}"
+            // }
+            // if (partFive){
+            //     apiCall += ",{%22mpn%22:%22"+partFive+"%22}"
+            // }
             // apiCall = "https://octopart.com/api/v3/parts/match?apikey=80dfab31&queries=%5b%7b%22mpn%22:%22"+partOne+"%22%7d%5d&include%5b%5d=specs,compliance_documents,&callback=?";
             // apiCall = "https://octopart.com/api/v3/parts/match?apikey=80dfab31&queries=%5b%7b%22mpn%22:%22"+partOne+"%22%7d%";
             // apiCall = "https://octopart.com/api/v3/parts/match?apikey=80dfab31&queries=[{%22mpn%22:%22SN74S74N%22},{%22mpn%22:%22MS15795-803%22}]&pretty_print=true"
             // end = "%5d&include%5b%5d=specs,compliance_documents,&callback=?";
             end = "]&callback=?"
-            apiCall += end;
+            apiCallOne += end;
              
-        $.getJSON(apiCall, function(resp) {
-            tableau.log(apiCall)
+        $.getJSON(apiCallOne, function(resp) {
+            tableau.log(apiCallOne)
             var feat = resp.results,
                 tableData = [];
                // tableau.log(feat[2].items[1].offers[0].prices.USD.length)
-               tableau.log('test')
-            // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                for (var j = 0, len2 = feat[i].items.length; j < len2; j++){
-                    for (var k = 0, len3 = feat[i].items[j].offers.length; k < len3; k++){
-                        if((typeof(feat[i].items[j].offers[k].prices) !== 'undefined') && (typeof(feat[i].items[j].offers[k].prices.USD) !== 'undefined')){
-                            for (var l = 0, len4 = feat[i].items[j].offers[k].prices.USD.length; l < len4; l++){
+            if(table.tableInfo.id == 'tableOne'){
+
+                 for (var i = 0, len = feat.length; i < len; i++) {
+                    for (var k = 0, len3 = feat[i].item.offers.length; k < len3; k++){
+                        if((typeof(feat[i].item.offers[k].prices) !== 'undefined') && (typeof(feat[i].item.offers[k].prices.USD) !== 'undefined')){
+                            for (var l = 0, len4 = feat[i].item.offers[k].prices.USD.length; l < len4; l++){
                             tableau.log("loop")
                             tableData.push({
-                                "mpn": feat[i].items[j].mpn,
-                                "brand_name": feat[i].items[j].brand.name,
-                                "manufacturer": feat[i].items[j].manufacturer.name,
-                                "sku" : feat[i].items[j].offers[k].sku,
-                                "seller_name" : feat[i].items[j].offers[k].seller.name,
-                                "USD" : feat[i].items[j].offers[k].prices.USD[l][1]
+                                "mpn": feat[i].item.mpn,
+                                "brand_name": feat[i].item.brand.name,
+                                "manufacturer": feat[i].item.manufacturer.name,
+                                "sku" : feat[i].item.offers[k].sku,
+                                "seller_name" : feat[i].item.offers[k].seller.name,
+                                "USD" : feat[i].item.offers[k].prices.USD[l][1]
                                    
                                 });
                             }
-                        }
-
-                        
-                    }
-                    
+                        }            
                 }
                 
             }
+            }
+            
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+
+
+        if (partTwo){
+            apiCallTwo = "http://octopart.com/api/v3/parts/search?apikey=80dfab31&q=%27"+partTwo+ "%27"
+            apiCallTwo += end;
+            $.getJSON(apiCallTwo, function(resp) {
+            tableau.log(apiCallTwo)
+            var feat = resp.results,
+                tableData = [];
+               // tableau.log(feat[2].items[1].offers[0].prices.USD.length)
+            if(table.tableInfo.id == 'tableTwo'){
+                
+                 for (var i = 0, len = feat.length; i < len; i++) {
+                    for (var k = 0, len3 = feat[i].item.offers.length; k < len3; k++){
+                        if((typeof(feat[i].item.offers[k].prices) !== 'undefined') && (typeof(feat[i].item.offers[k].prices.USD) !== 'undefined')){
+                            for (var l = 0, len4 = feat[i].item.offers[k].prices.USD.length; l < len4; l++){
+                            tableau.log("loop")
+                            tableData.push({
+                                "mpn": feat[i].item.mpn,
+                                "brand_name": feat[i].item.brand.name,
+                                "manufacturer": feat[i].item.manufacturer.name,
+                                "sku" : feat[i].item.offers[k].sku,
+                                "seller_name" : feat[i].item.offers[k].seller.name,
+                                "USD" : feat[i].item.offers[k].prices.USD[l][1]
+                                   
+                                });
+                            }
+                        }            
+                }
+                
+            }
+            }
+            // Iterate over the JSON object
+           
 
            
                 
@@ -105,6 +167,11 @@
             table.appendRows(tableData);
             doneCallback();
         });
+
+            }
+            tableau.log('call one and two')
+        tableau.log(apiCallTwo)
+        tableau.log(apiCallOne)
     };
 
     tableau.registerConnector(myConnector);
