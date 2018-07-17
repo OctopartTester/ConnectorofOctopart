2aea9731ec3f1eeb8a9d9941a887631bd4585c73
OctopartTester <40574972+OctopartTester@users.noreply.github.com>
2018-07-17 11:48:41 -0600

    Add files via upload

--- octopartWDC.js
+++ octopartWDC.js
@@ -99,9 +99,9 @@
             
             table.appendRows(tableData1);
             tableau.log('end first getJSON')
-
+doneCallback();
         });
-        
+        doneCallback();
        
 
         if (partTwo){
@@ -134,7 +134,7 @@
             }
 
             table.appendRows(tableData2);
-
+doneCallback();
         });
             doneCallback();
             }
