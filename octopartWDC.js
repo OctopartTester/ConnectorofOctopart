d34290b5e5dd67450f687b3a2e94f4717d80651f
OctopartTester <40574972+OctopartTester@users.noreply.github.com>
2018-07-18 10:38:37 -0600

    Add files via upload

--- octopartWDC.js
+++ octopartWDC.js
@@ -144,7 +144,7 @@
                 
                 table.appendRows(tableData1);
                 tableau.log('end first getJSON')
-                
+                doneCallback();
             });
                 apiCallTwo = "https://octopart.com/api/v3/parts/search?apikey=80dfab31&q=%27"+partTwo+ "%27"
                 apiCallTwo += end;
